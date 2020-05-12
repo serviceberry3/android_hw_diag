76e333df9fdf258d88217627f09fe56b36b678c8
nodog <noah.weiner@yale.edu>
2020-05-12 15:45:26 -0400

    Display test is up. Time for lunch.

--- app/src/main/java/com/example/testapp/RunTestActivity.java
+++ app/src/main/java/com/example/testapp/RunTestActivity.java
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.hardware.camera2.CameraAccessException;
 import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.display.DisplayManager;
 import android.os.Bundle;
+import android.view.Display;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -29,19 +31,55 @@ public class RunTestActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 //check component type, run test
+                assert component != null;
                 switch (component) {
                     case "Camera": {cameraTest(); break;}
-                    //case "Display": {displayTest(); break;}
+                    case "Display": {displayTest(); break;}
                 }
             }
         });
+    }
 
-        //success
-        return;
+    //run the test on all connected displays
+    public void displayTest() {
+        if (printDisplayIDs()<0) {
+            Toast.makeText(RunTestActivity.this, "FAIL", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            Toast.makeText(RunTestActivity.this, "PASS", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public int printDisplayIDs() {
+            //get a display manager object
+            DisplayManager displayManager = (DisplayManager) getSystemService(DISPLAY_SERVICE);
+
+            assert displayManager != null;
+
+            Display[] displays = displayManager.getDisplays();
+
+            //if there are no connected displays, problem
+            if (displays.length == 0) {
+                return -1;
+            }
+
+            //else go through displays and try to get display information
+            for (Display thisDisplay : displayManager.getDisplays()) {
+                int displayId = thisDisplay.getDisplayId();
+                Toast.makeText(RunTestActivity.this, "Trying Display #" + String.format("%d", displayId), Toast.LENGTH_SHORT).show();
+
+                //run a getDisplay check on the displayId, which will return Display object on success, or "null if there is no valid display with the given id"
+                //https://developer.android.com/reference/android/hardware/display/DisplayManager#getDisplay(int)
+                Display result = displayManager.getDisplay(displayId);
+                if (result == null) {
+                    return -1;
+                }
+            }
+        return 0;
     }
 
     //run the test on all cameras
-    public int cameraTest() {
+    public void cameraTest() {
         //Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE); -- this is a way to take a picture, which would probably work for the test, but I went with something more base
         //startActivityForResult(intent, 0);
         if (printCameraIDs()<0) {
@@ -50,7 +88,6 @@ public class RunTestActivity extends AppCompatActivity {
         else {
             Toast.makeText(RunTestActivity.this, "PASS", Toast.LENGTH_SHORT).show();
         }
-        return 0;
     }
 
     //get list of camera IDs, print out each one
